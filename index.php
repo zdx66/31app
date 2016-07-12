19fcd2ff4ef3b23560527662c4579502b7dd40a7
zdx66 <13675627043@163.com>
2016-07-12 18:05:30 +0800

    w

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-echo 'dd';return;
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
@@ -14,7 +13,6 @@ echo 'dd';return;
 
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
-
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
 // 定义应用目录
