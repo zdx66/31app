3c431675043f57257622c5b69947e96a8ececac3
zdx66 <13675627043@163.com>
2016-07-14 17:33:38 +0800

    9

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -205,10 +205,11 @@ class IndexController extends RestController
     {
         $imgData    =   I('post.');
         $user_id    =   $imgData['user_id'];
-        
-        $imgpath = $_SERVER['DOCUMENT_ROOT']."/Public/Uploads/";//.date("Y-m-d",time()).'/'
+        $upload = new \Think\Upload();
+        //$imgpath = $_SERVER['DOCUMENT_ROOT']."/Public/Uploads/";//.date("Y-m-d",time()).'/'
         //$PHP_SELF = $_SERVER['PHP_SELF'];
-        //$imgpath = 'http://'.$_SERVER['HTTP_HOST'].substr($PHP_SELF,0,  strrpos($PHP_SELF, '/')+1)."/Public/Uploads/";
+        dump(__FILE__);
+        $imgpath = 'http://'.$_SERVER['HTTP_HOST'].substr($PHP_SELF,0,  strrpos($PHP_SELF, '/')+1)."/Public/Uploads/";
         dump($imgpath);
         if(is_dir($imgpath))
         {
