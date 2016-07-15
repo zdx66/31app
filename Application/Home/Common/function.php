02846658e2583f5d534feba6dcd693d165a955c6
zdx66 <13675627043@163.com>
2016-07-15 16:49:07 +0800

    更新用户头像信息

--- Application/Home/Common/function.php
+++ Application/Home/Common/function.php
@@ -8,9 +8,45 @@ function is_base64_encoded($data){
 
 function is_cellphone($cellphone = '')
 {
-    if(!preg_match("/^13[0-9]{1}[0-9]{8}$|14[0-9]{1}[0-9]{8}$|17[0-9]{1}[0-9]{8}$|15[0189]{1}[0-9]{8}$|189[0-9]{8}$/",$cellphone)){
-        return FALSE;
+    if($cellphone){
+        if(preg_match("/^13[0-9]{1}[0-9]{8}$|14[0-9]{1}[0-9]{8}$|17[0-9]{1}[0-9]{8}$|15[0189]{1}[0-9]{8}$|189[0-9]{8}$/",$cellphone)){
+            return TRUE;
+        }
     }
-    return TRUE;
+    return false;
 
+}
+
+function is_email($email = '')
+{
+    if($email){
+        $pattern = "/^([0-9A-Za-z\\-_\\.]+)@([0-9a-z]+\\.[a-z]{2,3}(\\.[a-z]{2})?)$/i";   
+        if(!preg_match($pattern, $email )){
+            $data = array(
+                'code'  =>  '201',
+                'msg'   =>  '邮箱格式不合法'
+            );
+            exit(json_encode($data));
+        }
+    }
+    return FALSE;
+}
+
+//将图片解码并存到数据库，服务器中
+function deal_image($avatar)
+{
+            //把图片放在服务器上
+            $time = date("Y-m", time());
+            $imgpath = $_SERVER['DOCUMENT_ROOT'].'/'."Public/Uploads/".$time.'/';
+            if(!is_dir($imgpath)){
+                if(!mkdir($imgpath,0777,true))
+                {         
+                    echo "无法创建该路径";
+                }
+            }
+            //将图片存到服务器上
+            $imgname = time();
+            file_put_contents ($imgpath.$imgname.".jpg", $data['avatar'], FILE_USE_INCLUDE_PATH);
+            //将新的图片路径放到数据库中
+            $data['avatar'] = $imgpath.$imgname.".jpg";
 }
