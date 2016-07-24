fdaa4d64b412b25c2a72ab4dd359a826b43ddd49
zdx66 <13675627043@163.com>
2016-07-24 17:15:49 +0800

    登录接口

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -3,7 +3,28 @@ namespace Home\Controller;
 use Think\Controller;
 class LoginController extends Controller{
     
-    function Index(){
-        echo "here is Login Index";
+    function log(){
+        
+        $cellphone = I('post.cellphone');
+        $password = I('post.password');
+        $password = md5(md5($password));
+        $user = D('User');
+        $res = $user->where(array('cellphone'=>$cellphone,'password_hash'=>$password))->find();
+        unset($res['password_hash']);
+        if(!$res){
+            $str = array(
+                'code'  =>  '201',
+                'msg'   =>  '用户名或密码不正确'
+            );
+            exit(json_encode($str));
+        }else{
+            $str = array(
+                'data'  =>  $res,
+                'msg'   =>  '登录成功',
+                'code'  =>  '200'
+            );
+            exit(json_encode($str));
+        }
+        
     }
 }
