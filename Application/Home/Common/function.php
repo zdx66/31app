88529a5dc6b1f2e1cf9ae72000b3789980cf3765
zdx66 <13675627043@163.com>
2016-07-25 13:51:39 +0800

    33

--- Application/Home/Common/function.php
+++ Application/Home/Common/function.php
@@ -8,8 +8,8 @@ function is_base64_encoded($data){
 
 function is_cellphone($cellphone = '')
 {
-    if($cellphone){
-        if(preg_match("/^13[0-9]{1}[0-9]{8}$|14[0-9]{1}[0-9]{8}$|17[0-9]{1}[0-9]{8}$|15[0189]{1}[0-9]{8}$|189[0-9]{8}$/",$cellphone)){
+    if($cellphone){///^13[0-9]{1}[0-9]{8}$|14[0-9]{1}[0-9]{8}$|17[0-9]{1}[0-9]{8}$|15[0189]{1}[0-9]{8}$|189[0-9]{8}$/
+        if(preg_match("/^1[3,4,5,7,8]\d{9}$/",$cellphone)){
             return TRUE;
         }
     }
