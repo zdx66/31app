f8dc7d2e07a1e3d3d267f41ceac4fc0c91b9e4fd
zdx66 <13675627043@163.com>
2016-07-25 17:58:34 +0800

    2

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -449,7 +449,7 @@ class IndexController extends RestController
      */
     public function hx_contacts_delete()
     {
-        $data = I('post.owname');
+        $data = I('post.owname');dump($data);
         $owner_username = $data['owname'];
         $friend_username = $data['fname'];
         $friend = D('friends');
